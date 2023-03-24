4961ec9da5fd1293bdb9ebb01d79fb72e54c64b5
actiONmycoma <actionmycoma@gmail.com>
2023-03-24 15:23:26 +1000

    Корректировка перечисления и метода InitCardsArr

--- Find Pair Card Game/Program.cs
+++ Find Pair Card Game/Program.cs
@@ -15,8 +15,7 @@ namespace Find_Pair_Card_Game
             PosY,
             Width,
             Height,
-            ImageId,
-            AmountData
+            ImageId            
         }
 
         static int[,] cardsArr;
@@ -28,22 +27,7 @@ namespace Find_Pair_Card_Game
         static int bottomOffset = 20;
         static int space = 30;
         static int cardsInLine = 5;
-
-        static void InitCardsArr()
-        {
-            cardsArr = new int[amountCards, (int)CardData.AmountData];
-
-            for (int i = 0; i < cardsArr.GetLength(0); i++)
-            {
-                cardsArr[i, (int)CardData.State] = 1;
-                cardsArr[i, (int)CardData.PosX] = (i % cardsInLine) * (cardWidth + space) + bottomOffset;
-                cardsArr[i, (int)CardData.PosY] = (i / cardsInLine) * (cardHeigth + space) + topOffset;
-                cardsArr[i, (int)CardData.Width] = cardWidth;
-                cardsArr[i, (int)CardData.Height] = cardHeigth;
-                cardsArr[i, (int)CardData.ImageId] = 0;
-
-            }
-        }
+        
 
         static void Main(string[] args)
         {
@@ -55,11 +39,11 @@ namespace Find_Pair_Card_Game
             {
                 DispatchEvents();
 
-                //for (int i = 0; i < cardsArr.GetLength(0); i++)
-                //{
-                //    FillRectangle(cardsArr[i, (int)CardData.PosX], cardsArr[i,(int)CardData.PosY],
-                //        cardsArr[i,(int)CardData.Width], cardsArr[i,(int)CardData.Height]);
-                //}
+                for (int i = 0; i < cardsArr.GetLength(0); i++)
+                {
+                    FillRectangle(cardsArr[i, (int)CardData.PosX], cardsArr[i, (int)CardData.PosY],
+                        cardsArr[i, (int)CardData.Width], cardsArr[i, (int)CardData.Height]);
+                }
 
                 DisplayWindow();
 
@@ -67,5 +51,23 @@ namespace Find_Pair_Card_Game
             }
 
         }
+
+        static void InitCardsArr()
+        {
+            cardsArr = new int[amountCards, Enum.GetValues(typeof(CardData)).Length];
+             
+
+            for (int i = 0; i < cardsArr.GetLength(0); i++)
+            {
+                cardsArr[i, (int)CardData.State] = 1;
+                cardsArr[i, (int)CardData.PosX] = (i % cardsInLine) * (cardWidth + space) + bottomOffset;
+                cardsArr[i, (int)CardData.PosY] = (i / cardsInLine) * (cardHeigth + space) + topOffset;
+                cardsArr[i, (int)CardData.Width] = cardWidth;
+                cardsArr[i, (int)CardData.Height] = cardHeigth;
+                cardsArr[i, (int)CardData.ImageId] = 0;
+            }
+        }
+
+
     }
 }
