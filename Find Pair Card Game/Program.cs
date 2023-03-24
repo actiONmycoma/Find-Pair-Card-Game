f8ca36a8204fd01003909d6f4b78cd5b35ef8a4b
actiONmycoma <actionmycoma@gmail.com>
2023-03-24 14:07:26 +1000

    Add project files.

---
+++ Find Pair Card Game/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SFML.System;
+using SFML.Learning;
+using SFML.Window;
+
+
+namespace Find_Pair_Card_Game
+{
+    internal class Program : Game
+    {
+        static void Main(string[] args)
+        {
+            InitWindow(800, 600, "Найди пару");
+
+            while (true) 
+            {
+                DispatchEvents();
+
+                DisplayWindow();
+
+                Delay(1);
+            }
+
+        }
+    }
+}
